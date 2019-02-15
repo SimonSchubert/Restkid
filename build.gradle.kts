147e18715d2a0bccfe8ffbdc32aa6e0c6622b7fa
Simon Schubert <sschubert89@gmail.com>
2019-02-15 19:53:17 +0100

    Init

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("kotlin-multiplatform") version "1.3.21"
+    id("kotlinx-serialization") version "1.3.21"
+}
+
+repositories {
+    jcenter()
+    maven("https://kotlin.bintray.com/kotlinx")
+    maven("https://kotlin.bintray.com/ktor")
+}
+
+val os = org.gradle.internal.os.OperatingSystem.current()!!
+
+kotlin {
+    when {
+        os.isWindows -> mingwX64("libui")
+        os.isMacOsX -> macosX64("libui")
+        os.isLinux -> linuxX64("libui")
+        else -> throw Error("Unknown host")
+    }.binaries.executable {
+
+    }
+    val libuiMain by sourceSets.getting {
+        dependencies {
+            implementation("com.github.msink:libui:0.1.2")
+            implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-native:0.10.0")
+            // implementation("io.ktor:ktor-http-native:1.1.2")
+            // implementation("io.ktor:ktor-client-curl:1.1.2")
+            // implementation("io.ktor:ktor-client-core-native:1.1.2")
+        }
+    }
+
+    iosX64 {
+        binaries.all {
+            linkerOpts = mutableListOf("-L/usr/local/opt/curl/lib", "-I/usr/local/opt/curl/include")
+        }
+    }
+}
---
+++ src/libuiMain/kotlin/App.kt
@@ -0,0 +1,235 @@
+import libui.ktx.*
+import kotlinx.cinterop.*
+import platform.posix.*
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+// import io.ktor.client.HttpClient
+// import io.ktor.client.call.call
+// import io.ktor.client.engine.curl.Curl
+// import kotlinx.coroutines.GlobalScope
+// import kotlinx.coroutines.launch
+
+@Serializable
+data class Api(
+        var info: ApiInfo,
+        @SerialName("item")
+        var groups: List<RequestGroup>)
+
+@Serializable
+data class ApiInfo(
+        @Optional
+        var name: String = "",
+        @Optional
+        var description: String = "")
+
+@Serializable
+data class RequestGroup(
+        @Optional
+        var name: String = "",
+        @Optional
+        var description: String = "",
+        @SerialName("item")
+        var items: List<RequestItem>
+)
+
+@Serializable
+data class RequestItem(
+        @Optional
+        var name: String = "",
+        @Optional
+        var request: Request = Request()
+)
+
+@Serializable
+data class Request(
+        // val url: URLUnion? = null,
+        @Optional
+        var url: String = "",
+        @Optional
+        var method: String = "",
+        @Optional
+        var description: String = "",
+        @Optional
+        var body: String = "",
+        @Optional
+        @SerialName("header")
+        var headers: List<RequestItemHeader> = listOf<RequestItemHeader>()
+)
+
+@Serializable
+data class RequestItemHeader(
+        @Optional
+        var key: String = "",
+        @Optional
+        var value: String = "",
+        @Optional
+        var description: String = ""
+)
+
+sealed class URLUnion {
+    class StringValue(val u: String) : URLUnion()
+    class URLClassValue(val u: URLClass) : URLUnion()
+}
+
+data class URLClass(
+        val raw: String? = null
+)
+
+val data = listOf<Api>()
+
+fun main() = appWindow(
+        title = "Restkid",
+        width = 620,
+        height = 300
+) {
+    hbox {
+        var importPath: String
+        var box: VBox? = null
+        var uiUrl: TextField? = null
+        var uiMethod: Combobox? = null
+        var uiHeaders: TextArea? = null
+        var uiBody: TextArea? = null
+
+        vbox {
+            hbox {
+                button("Import") {
+                    action {
+                        importPath = OpenFileDialog() ?: ""
+                        if (importPath.isNotEmpty()) {
+                            val file = fopen(importPath, "r")
+                            try {
+                                var content = ""
+                                memScoped {
+                                    val bufferLength = 64 * 1024
+                                    val buffer = allocArray<ByteVar>(bufferLength)
+                                    while (true) {
+                                        val nextLine = fgets(buffer, bufferLength, file)?.toKString()
+                                        if (nextLine == null || nextLine.isEmpty()) break
+                                        content += nextLine
+                                    }
+
+
+                                    val obj: Api = Json.nonstrict.parse(Api.serializer(), content)
+                                    println(obj.info.name)
+
+                                    for (g in obj.groups) {
+                                        box?.label(g.name)
+                                        for (item in g.items) {
+                                            val url = item.request.url
+                                            val method = item.request.method
+                                            println("url: $url $method")
+                                            box?.button(item.name) {
+                                                action {
+                                                    println("url: $url")
+                                                    uiUrl?.value = url
+                                                    uiMethod?.value = if (method == "GET") {
+                                                        0
+                                                    } else {
+                                                        1
+                                                    }
+                                                    uiHeaders?.value = ""
+                                                    for (header in item.request.headers) {
+                                                        uiHeaders?.append(header.key + ": " + header.value + "\n")
+                                                    }
+                                                    uiBody?.value = item.request.body
+                                                }
+                                            }
+                                        }
+                                    }
+
+                                }
+                            } finally {
+                                fclose(file)
+                            }
+                        }
+                    }
+                }
+                button("Variables") {
+
+                }
+            }
+
+            separator()
+
+            box = vbox {
+            }
+        }
+
+        separator()
+        stretchy = true
+
+        vbox {
+            hbox {
+                uiMethod = combobox {
+                    item("GET")
+                    item("POST")
+                }
+                uiUrl = textfield() {
+                    stretchy = true
+                }
+                button("SEND") {
+
+                    memScoped {
+                        /*
+                        val client = HttpClient(Curl)
+
+                        val req1 = client.call("http://example.com").response.content.readByte()
+                        // val bytes1 = req1.await()
+                        printf("bytes: $req1")
+                        client.close()
+                        */
+                    }
+                    // */
+
+                    /*
+                    val req1 = async { client1.call("http://example.com").response.readBytes() }
+                    val bytes1 = req1.await()
+                    printf("bytes: $bytes1")
+                    client1.close()
+                    */
+
+                    /*
+                    val curl = curl_easy_init()
+                    printf("curl: " (curl != null))
+                    if (curl != null) {
+                        curl_easy_setopt(curl, CURLOPT_URL, "http://example.com")
+                        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L)
+                        val res = curl_easy_perform(curl)
+                        if (res != CURLE_OK) {
+                            println("curl_easy_perform() failed ${curl_easy_strerror(res)?.toKString()}")
+                        }
+                        curl_easy_cleanup(curl)
+                    }
+                    */
+                }
+            }
+
+            /*
+            tabpane {
+                page("Headers") {
+                    form {
+                    }
+                }
+                page("Body") {
+                }
+            }
+            */
+
+            group("Data") { stretchy = true }.form {
+                uiHeaders = textarea {
+                    label = "Headers"
+                    stretchy = true
+                }
+                uiBody = textarea {
+                    label = "Body"
+                    stretchy = true
+                }
+            }
+            group("Response") { stretchy = true }.form {
+                textarea {
+                    stretchy = true
+                }
+            }
+        }
+    }
+}
