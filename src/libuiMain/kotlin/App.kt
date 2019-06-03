094bd87862949771988e080566a1dcc8440fe80b
Simon Schubert <sschubert89@gmail.com>
2019-06-03 20:11:51 +0200

    Add working ktor setup

--- src/libuiMain/kotlin/App.kt
+++ src/libuiMain/kotlin/App.kt
@@ -3,11 +3,15 @@ import kotlinx.cinterop.*
 import platform.posix.*
 import kotlinx.serialization.*
 import kotlinx.serialization.json.*
-// import io.ktor.client.HttpClient
-// import io.ktor.client.call.call
-// import io.ktor.client.engine.curl.Curl
-// import kotlinx.coroutines.GlobalScope
-// import kotlinx.coroutines.launch
+import io.ktor.client.HttpClient
+import io.ktor.client.call.call
+import io.ktor.client.call.receive
+import io.ktor.client.engine.curl.Curl
+import io.ktor.client.response.HttpResponse
+import io.ktor.client.response.readText
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import kotlinx.coroutines.*
 
 @Serializable
 data class Api(
@@ -63,74 +67,35 @@ data class URLClass(
 
 val data = listOf<Api>()
 
+
+val client: HttpClient by lazy {
+    HttpClient(Curl)
+}
+
+var box: VBox? = null
+var uiUrl: TextField? = null
+var uiMethod: Combobox? = null
+var uiHeaders: TextArea? = null
+var uiBody: TextArea? = null
+var uiResponse: TextArea? = null
+
 fun main() = appWindow(
         title = "Restkid",
         width = 620,
         height = 300
 ) {
     hbox {
-        var importPath: String
-        var box: VBox? = null
-        var uiUrl: TextField? = null
-        var uiMethod: Combobox? = null
-        var uiHeaders: TextArea? = null
-        var uiBody: TextArea? = null
-
         vbox {
             hbox {
                 button("Import") {
                     action {
-                        importPath = OpenFileDialog() ?: ""
-                        if (importPath.isNotEmpty()) {
-                            val file = fopen(importPath, "r")
-                            try {
-                                var content = ""
-                                memScoped {
-                                    val bufferLength = 64 * 1024
-                                    val buffer = allocArray<ByteVar>(bufferLength)
-                                    while (true) {
-                                        val nextLine = fgets(buffer, bufferLength, file)?.toKString()
-                                        if (nextLine == null || nextLine.isEmpty()) break
-                                        content += nextLine
-                                    }
-
-
-                                    val obj: Api = Json.nonstrict.parse(Api.serializer(), content)
-                                    println(obj.info.name)
-
-                                    for (g in obj.groups) {
-                                        box?.label(g.name)
-                                        for (item in g.items) {
-                                            val url = item.request.url
-                                            val method = item.request.method
-                                            println("url: $url $method")
-                                            box?.button(item.name) {
-                                                action {
-                                                    println("url: $url")
-                                                    uiUrl?.value = url
-                                                    uiMethod?.value = if (method == "GET") {
-                                                        0
-                                                    } else {
-                                                        1
-                                                    }
-                                                    uiHeaders?.value = ""
-                                                    for (header in item.request.headers) {
-                                                        uiHeaders?.append(header.key + ": " + header.value + "\n")
-                                                    }
-                                                    uiBody?.value = item.request.body
-                                                }
-                                            }
-                                        }
-                                    }
-
-                                }
-                            } finally {
-                                fclose(file)
-                            }
-                        }
+                        importCollection()
                     }
                 }
-                button("Variables") {
+                button("Environment") {
+
+                }
+                button("Add") {
 
                 }
             }
@@ -149,57 +114,21 @@ fun main() = appWindow(
                 uiMethod = combobox {
                     item("GET")
                     item("POST")
+
+                    action {
+                        uiBody?.visible = value != 0
+                    }
                 }
-                uiUrl = textfield() {
+                uiUrl = textfield {
                     stretchy = true
                 }
                 button("SEND") {
 
-                    memScoped {
-                        /*
-                        val client = HttpClient(Curl)
-
-                        val req1 = client.call("http://example.com").response.content.readByte()
-                        // val bytes1 = req1.await()
-                        printf("bytes: $req1")
-                        client.close()
-                        */
+                    action {
+                        makeRequest()
                     }
-                    // */
-
-                    /*
-                    val req1 = async { client1.call("http://example.com").response.readBytes() }
-                    val bytes1 = req1.await()
-                    printf("bytes: $bytes1")
-                    client1.close()
-                    */
-
-                    /*
-                    val curl = curl_easy_init()
-                    printf("curl: " (curl != null))
-                    if (curl != null) {
-                        curl_easy_setopt(curl, CURLOPT_URL, "http://example.com")
-                        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L)
-                        val res = curl_easy_perform(curl)
-                        if (res != CURLE_OK) {
-                            println("curl_easy_perform() failed ${curl_easy_strerror(res)?.toKString()}")
-                        }
-                        curl_easy_cleanup(curl)
-                    }
-                    */
                 }
             }
-
-            /*
-            tabpane {
-                page("Headers") {
-                    form {
-                    }
-                }
-                page("Body") {
-                }
-            }
-            */
 
             group("Data") { stretchy = true }.form {
                 uiHeaders = textarea {
@@ -211,11 +140,101 @@ fun main() = appWindow(
                     stretchy = true
                 }
             }
+
             group("Response") { stretchy = true }.form {
-                textarea {
+                uiResponse = textarea {
                     stretchy = true
                 }
             }
         }
     }
+}
+
+private fun makeRequest() {
+    memScoped {
+        runBlocking {
+            try {
+                val url = uiUrl?.value ?: ""
+
+                val call = client.call(url) {
+                    method = if (uiMethod?.value == 0) {
+                        HttpMethod.Get
+                    } else {
+                        HttpMethod.Post
+                    }
+                    uiHeaders?.value?.lines()?.forEach {
+                        val dividerIndex = it.indexOf(":")
+                        if (dividerIndex != -1) {
+                            println("h: $it $dividerIndex")
+                            headers.append(it.substring(0, dividerIndex), it.substring(dividerIndex))
+                        }
+                    }
+                }
+
+                val response = call.response.receive<HttpResponse>()
+
+
+                if (response.status == HttpStatusCode.OK) {
+                }
+
+                uiResponse?.value = response.readText()
+
+                response.close()
+            } catch (e : Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+private fun importCollection() {
+    val importPath = OpenFileDialog() ?: ""
+    if (importPath.isNotEmpty()) {
+        val file = fopen(importPath, "r")
+        try {
+            var content = ""
+            memScoped {
+                val bufferLength = 64 * 1024
+                val buffer = allocArray<ByteVar>(bufferLength)
+                while (true) {
+                    val nextLine = fgets(buffer, bufferLength, file)?.toKString()
+                    if (nextLine == null || nextLine.isEmpty()) break
+                    content += nextLine
+                }
+
+                val obj: Api = Json.nonstrict.parse(Api.serializer(), content)
+                println(obj.info.name)
+
+                for (g in obj.groups) {
+                    box?.label(g.name)
+                    for (item in g.items) {
+                        box?.button(item.name) {
+                            action {
+                                fillRequestData(item.request.url, item.request.method, item.request.headers, item.request.body)
+                            }
+                        }
+                    }
+                }
+            }
+        } finally {
+            fclose(file)
+        }
+    }
+}
+
+private fun fillRequestData(url: String, method: String, headers: List<RequestItemHeader>, body: String) {
+    println("url: $url")
+    uiUrl?.value = url
+    uiMethod?.value = if (method == "GET") {
+        uiBody?.hide()
+        0
+    } else {
+        uiBody?.show()
+        1
+    }
+    uiHeaders?.value = ""
+    for (header in headers) {
+        uiHeaders?.append(header.key + ": " + header.value + "\n")
+    }
+    uiBody?.value = body
 }
