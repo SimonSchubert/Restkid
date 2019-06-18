4be0f1046504399fefc71420d518298d5b191cc4
Simon Schubert <sschubert89@gmail.com>
2019-06-18 21:20:57 +0200

    Upgrade libui to 0.1.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ kotlin {
     }
     val desktopMain by sourceSets.getting {
         dependencies {
-            implementation("com.github.msink:libui:0.1.3")
+            implementation("com.github.msink:libui:0.1.4")
             implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-native:0.11.0")
             implementation("io.ktor:ktor-client-core-native:1.2.1")
             implementation("io.ktor:ktor-client-curl:1.2.1")
